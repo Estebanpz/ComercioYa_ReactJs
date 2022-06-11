8aa9d19b6927dd8862a872428c317e9db69868d9
EstebanPz <estebanpl81@gmail.com>
2022-06-10 23:51:47 -0500

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import styled from "styled-components";
 //Iconos
-import { BsDoorOpen, BsPersonPlus, BsHouse, BsShop, BsDoorClosedFill, BsDoorOpenFill } from "react-icons/bs";
+import { BsDoorOpen, BsPersonPlus, BsHouse, BsShop, BsFillDoorClosedFill } from "react-icons/bs";
 //Importando el boton de cerrar sesion
 
 // Importando la conexion a el contexto de User
-import { useAuth } from "../Context/AuthContext";
+import { useAuth} from "../Context/AuthContext";
+
 const Header = () => {
   const { user } = useAuth();
-
   return (
     <header className="container-fluid py-1">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded">
@@ -53,7 +52,7 @@ const Header = () => {
                       className="navbar-brand d-flex align-items-center"
                     >
                         <span className="mx-1">
-                          <BsPersonPlus fontSize="1.9rem" />
+                          <BsDoorOpen fontSize="1.9rem" />
                         </span>
                       Iniciar Sesion
                     </NavLink>
@@ -62,18 +61,18 @@ const Header = () => {
               </li>
               <li className="nav-item">
                  {
-                  user ? (
-                    <NavLink to="/" className="nav-link d-flex align-items-center">
+                   user ? (
+                     <NavLink to="/" className="nav-link d-flex align-items-center">
                         <span className="mx-1">
-                          <BsDoorOpenFill fontSize="1.9rem" /> 
+                          <BsFillDoorClosedFill fontSize="1.9rem" /> 
                         </span>
                       Salir
                     </NavLink>
                   ):
                   (
-                    <NavLink to="/" className="nav-link d-flex align-items-center">
+                    <NavLink to="/crear-cuenta" className="nav-link d-flex align-items-center">
                         <span className="mx-1">
-                          <BsDoorOpen fontSize="1.9rem" />
+                          <BsPersonPlus fontSize="1.9rem" />
                         </span>
                         Registrate
                     </NavLink>
